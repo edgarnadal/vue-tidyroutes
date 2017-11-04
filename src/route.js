11161646a48b3ee48b25d9680189341bb9f434ef
Edgar Nadal <edgar.nadal@gmail.com>
2017-11-04 01:41:58 -0400

    routes collection

---
+++ src/route.js
@@ -0,0 +1,25 @@
+class Route {
+
+    constructor(path, options) {
+
+        this.path = path
+        this.options = options
+        this.children = {}
+
+        if (options.hasOwnProperty('children')) {
+
+            for(let path in options.children) {
+                this.child(path, options.children[path])
+            }
+
+            delete options.children
+        }
+    }
+
+    child(path, options) {
+        this.children[path] = options
+        return this
+    }
+}
+
+export default Route
---
+++ src/tidy-routes.js
@@ -0,0 +1,56 @@
+import Route from './route'
+
+let __tidyRoutesInstance = null
+
+class TidyRoutes {
+
+    constructor() {
+
+        if (!__tidyRoutesInstance) {
+            this.routes = new Map()
+            __tidyRoutesInstance = this
+        }
+
+        return __tidyRoutesInstance
+    }
+
+    export() {
+
+        let routes = []
+
+        this.routes.forEach((option, path) => {
+
+            let route = this.routes.get(path)
+            let options = route.options
+
+            options.children = []
+
+            for (let path in route.children) {
+                let child = route.children[path]
+
+                options.children.push(Object.assign({
+                    path: path
+                }, child))
+            }
+
+            routes.push(Object.assign({ path: path }, options))
+        })
+
+        return routes
+    }
+
+    path(path, options) {
+
+        if (this.routes.has(path)) {
+            return this.routes.get(path)
+        }
+
+        let route = new Route(path, options)
+
+        this.routes.set(path, route)
+
+        return route
+    }
+}
+
+export default TidyRoutes
